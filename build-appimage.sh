8d9c9a9aeadda1e6d984f448539533eff08c2e35
TheAssassin <theassassin@users.noreply.github.com>
2017-02-12 00:39:57 +0100

    Red Eclipse release 1.5.8

--- build-appimage.sh
+++ build-appimage.sh
@@ -25,8 +25,8 @@ export LC_ALL=C
 export LANGUAGE=C
 
 export APP=redeclipse
-export VERSION=1.5.6
-export BRANCH=stable
+export VERSION=1.5.8
+export BRANCH=v1.5.8
 export ARCH=x86_64
 
 export WORKSPACE=/workspace
